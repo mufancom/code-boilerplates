b6ea3a5a2ceceb939711bfc55a3a8bad76845113
vilicvane <i@vilic.info>
2020-12-23 02:32:57 +0800

    Filter packages without entrances-enabled project

--- typescript-entrances/src/composables/package.json.ts
+++ typescript-entrances/src/composables/package.json.ts
@@ -2,23 +2,23 @@ import {ComposableModuleFunction, json} from '@magicspace/core';
 import {fetchPackageVersions} from '@magicspace/utils';
 import _ from 'lodash';
 
-import {resolveTypeScriptProjects} from '../../../typescript/bld/library';
+import {resolveTypeScriptProjectsWithEntrances} from './@utils';
 
 const DEPENDENCY_DICT = {
   'entrance-decorator': '0.1',
 };
 
 const composable: ComposableModuleFunction = async options => {
-  let {projects} = resolveTypeScriptProjects(options);
+  let projects = resolveTypeScriptProjectsWithEntrances(options);
 
-  let packagesWithTypeScriptProject = _.uniqBy(
+  let packagesWithEntrancesEnabledProject = _.uniqBy(
     projects.map(project => project.package),
     packageOptions => packageOptions.packageJSONPath,
   );
 
   let dependencies = await fetchPackageVersions(DEPENDENCY_DICT);
 
-  return packagesWithTypeScriptProject.map(packageOptions =>
+  return packagesWithEntrancesEnabledProject.map(packageOptions =>
     json(packageOptions.packageJSONPath, (data: any) => {
       return {
         ...data,
