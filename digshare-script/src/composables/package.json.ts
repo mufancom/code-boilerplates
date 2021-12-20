318d130a993dea4a7352c541949cc44aa508f35b
boenfu <1997@boenfu.cn>
2021-12-20 20:14:57 +0800

    修改依赖

--- digshare-script/src/composables/package.json.ts
+++ digshare-script/src/composables/package.json.ts
@@ -10,10 +10,10 @@ import {
 } from '../../../typescript/bld/library';
 
 const DEPENDENCY_DICT = {
+  '@digshare/script': '^0.1.0',
   '@types/cheerio': '^0.22.30',
   '@types/node-fetch': '^2.5.12',
   cheerio: '^1.0.0-rc.10',
-  'dss-sdk': '^0.3.0',
   'node-fetch': '2.6.5',
 };
 
