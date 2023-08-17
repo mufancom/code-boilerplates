8b9a7e019cbe4c9111ef4e670ecf24b857ab5f9c
vilicvane <vilicvane@live.com>
2023-08-18 02:49:46 +0800

    Add package manage gitignore files

--- general/src/composables/gitignore.ts
+++ general/src/composables/gitignore.ts
@@ -1,11 +1,40 @@
-import {text} from '@magicspace/core';
+import type {Composable, TextFile} from '@magicspace/core';
+import {composable, text} from '@magicspace/core';
 
-export default text(
-  '.gitignore',
-  `\
+import type {ResolvedOptions} from '../library';
+
+const PACKAGE_MANAGER_IGNORE_ENTRIES_DICT = {
+  pnpm: undefined,
+  yarn: ['yarn-error.log'],
+};
+
+export default composable<ResolvedOptions>(({packageManager}) => {
+  const composables: Composable<TextFile>[] = [
+    text(
+      '.gitignore',
+      `\
 # General
 .DS_Store
 *.tgz
 node_modules/
 `,
-);
+    ),
+  ];
+
+  const packageManagerIgnoreEntries =
+    PACKAGE_MANAGER_IGNORE_ENTRIES_DICT[packageManager];
+
+  if (packageManagerIgnoreEntries) {
+    composables.push(
+      text(
+        '.gitignore',
+        content => `${content}\
+# Package Manager
+${packageManagerIgnoreEntries.join('\n')}
+`,
+      ),
+    );
+  }
+
+  return composables;
+});
