4af09c9fa27f817491ce505573ff69ba809d12fa
vilicvane <vilicvane@live.com>
2023-11-01 22:48:45 +0800

    Clean up

--- general/src/library/boilerplate.ts
+++ general/src/library/boilerplate.ts
@@ -52,7 +52,7 @@ export type PackageType = x.TypeOf<typeof PackageType>;
 
 export const PackageOptions = x.object({
   name: x.string,
-  type: PackageType.optional(),
+  type: PackageType,
   dir: x.string
     .nominal({
       description:
